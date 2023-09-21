59cc6103a5c4224d50b59be5c1b68c691dd7ddb8
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-09-21 16:50:55 -0300

    feat: criando registros com POST

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+  {
+    id: 1,
+    título: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    título: "O Hobbit",
+  },
+];
 
 app.get("/", (req, res) => {
   res
     .status(200)
     .send("Curso de Node.js: criando uma API Rest com Express e MongoDB");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso");
 });
 
 export default app;
