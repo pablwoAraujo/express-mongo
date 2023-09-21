ed2e3d81cbd141b821e414277d2ca450e0351e9f
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-09-21 16:30:21 -0300

    feat: criando rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de Node.js: criando uma API Rest com Express e MongoDB",
+  "/livros" : "Livros recomendados",
+  "/autores" : "Autores recomendados",
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js: criando uma API Rest com Express e MongoDB");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
