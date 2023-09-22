9f897f0a2ed1de5482b1520b8fdbee612dc3bef1
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-09-22 19:07:06 -0300

    feat: cadastro e atualização dos livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,29 +3,64 @@ import livros from "../models/Livros.js";
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livros.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livros.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha na requisição` });
+    }
   }
 
   static async listarLivroPorId(req, res) {
     try {
       const id = req.params.id;
-      const livro = await livros.findById(id).exec();
+      const livro = await livros.findById(id);
 
-      if (livro === null){
+      if (livro === null) {
         throw new NotFoundError("Livro não encontrado na base de dados");
       }
       res.status(200).json(livro);
     } catch (erro) {
       if (erro instanceof NotFoundError) {
-        res
-        .status(404)
-        .json({ message: `${erro.message}` });
-      }
-      else{
+        res.status(404).json({ message: `${erro.message}` });
+      } else {
         res
           .status(500)
           .json({ message: `${erro.message} - falha na requisição do livro` });
+      }
+    }
+  }
+
+  static async cadastrarLivro(req, res) {
+    try {
+      const novoLivro = await livros.create(req.body);
+      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao cadastrar livro` });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livros.findByIdAndUpdate(id, req.body);
+      const livroAtualizado = await livros.findById(id);
+
+      if (livroAtualizado === null) {
+        throw new NotFoundError("Livro não encontrado na base de dados");
+      }
+      res.status(200).json({ message: "livro atualizado", livroAtualizado });
+    } catch (erro) {
+      if (erro instanceof NotFoundError) {
+        res.status(404).json({ message: `${erro.message}` });
+      } else {
+        res
+          .status(500)
+          .json({ message: `${erro.message} - falha na atualização` });
       }
     }
   }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,5 +5,7 @@ const livrosRoutes = express.Router();
 
 livrosRoutes.get("/livros", LivroController.listarLivros);
 livrosRoutes.get("/livros/:id", LivroController.listarLivroPorId);
+livrosRoutes.post("/livros", LivroController.cadastrarLivro);
+livrosRoutes.put("/livros/:id", LivroController.atualizarLivro);
 
 export default livrosRoutes;
