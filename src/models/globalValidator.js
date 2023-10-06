4a4eef42e48245a3cae94f7e56b50130ac9a4790
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-10-06 15:31:49 -0300

    refactor: adicionando a verificação de espaços inseridos no texto

--- src/models/globalValidator.js
+++ src/models/globalValidator.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.Schema.Types.String.set(
-  "validate", 
-  {
-    validator: (value) =>  value !== "",
-    message: ({path}) => `O campo _${path} não pode ficar em branco`
-  }
-);
+mongoose.Schema.Types.String.set("validate", {
+  validator: (value) => value.trim() !== "",
+  message: ({ path }) => `O campo _${path} não pode ficar em branco`,
+});
